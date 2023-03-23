0749652f231473fa765cb9c77131617a77ec4a43
Lucyanno Frota <lucyannofrota@gmail.com>
2023-03-23 18:51:11 +0000

    check and load model methods

--- smap_classification_wrapper/classification_wrapper.py
+++ smap_classification_wrapper/classification_wrapper.py
@@ -22,6 +22,11 @@ class classification_wrapper(Node):
     classes = []
     parameters = []
 
+    device=None
+    model=None
+    model_file=None
+    imgsz=None
+
     def __init__(self,classifier_name='Cl1',classifier_type='object',classifier_architecture='no_defined'):
         super().__init__("smap_classification_{}".format(classifier_name))
 
@@ -38,6 +43,7 @@ class classification_wrapper(Node):
         self.__ret_valid=10
 
         self.__shutdown_node=False
+
         self.add_classifier()
 
         self.valid_timer=self.create_timer(1.0,self.__validate_classifier)
@@ -70,6 +76,47 @@ class classification_wrapper(Node):
         self.get_logger().info("Sending request...")
         self.fut_val=self.cli.call_async(req)
 
+    def setup_detector(self):
+        # Check device
+        if self._chech_device():
+            return True
+
+        # Load model
+        if self._load_model():
+            return True
+        
+        return False
+
+    def _chech_device(self):
+        if self.device is None:
+            self.get_logger().fatal("Device not defined!")
+            self._shutdown()
+            return True
+        return False
+
+    def _load_model(self):
+        # Load model
+        if (
+            (self.model is None) or 
+            (self.model_file is None)
+            ):
+            self.get_logger().fatal("Model not defined!")
+            self._shutdown()
+            return True
+        
+
+        self.get_logger().info("Loading Model {}".format(self.model_file))
+
+
+
+        
+
+
+        #device = select_device(device)
+        #model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
+        #stride, names, pt = model.stride, model.names, model.pt
+        #imgsz = check_img_size(imgsz, s=stride)  # check image size
+        pass
 
     def __validate_classifier(self):
         if self.__shutdown_node==True:
@@ -108,7 +155,8 @@ class classification_wrapper(Node):
         
         self.classifier_id=resp.classifier_id
 
-        self.initialization()
+        if self.initialization():
+            return True
 
         self.get_logger().info("smap classifier:")
         self.get_logger().info("\t name: {}".format(self.classifier_name))
@@ -118,6 +166,11 @@ class classification_wrapper(Node):
         self.get_logger().info("smap wrapper launched!")
     
     def initialization(self):
+        if self.setup_detector():
+            return True
+        self._initialize_communication()
+    
+    def _initialize_communication(self):
         self.subscription=self.create_subscription(SmapData, '/smap/classifiers/data', self.predict, 10,callback_group= self.reentrant_cb_group)
         self.publisher=self.create_publisher(SmapPrediction, '/smap/classifiers/predictions', 10,callback_group= self.reentrant_cb_group)
 
@@ -130,10 +183,15 @@ class classification_wrapper(Node):
         msg.classifier_id=0
         self.publisher.publish(msg)
 
+    def _shutdown(self):
+        self.get_logger().fatal("Shutting down node!")
+        self.__shutdown_node = True
+        self.destroy_node()
+
     def on_process(self): # Pooling
+        self.load_model()
         if self.__shutdown_node:
             return True
-        pass
 
 def main(args=None,classifier_class=classification_wrapper,classifier_args={'name': 'classification_wrapper'}):
 
@@ -148,7 +206,6 @@ def main(args=None,classifier_class=classification_wrapper,classifier_args={'nam
         try:
             executor.spin_once()
             if classifier_obj.on_process():
-                classifier_obj.destroy_node()
                 executor.shutdown(0)
                 rclpy.shutdown()
                 return
